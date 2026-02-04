e4f1ddab1fa80eac829d324006c7abae6eb80495
5manuelapele@gmail.com <5manuelapele@gmail.com>
2026-02-04 19:45:48 +0000

    operationnal margin

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -2,16 +2,17 @@ WITH ship_join AS(
 SELECT 
 m.orders_id
 , m.date_date 
+, m.margin
 , s.shipping_fee 
 , s.logcost
 , s.ship_cost
-FROM int_orders_margin m
-LEFT JOIN stg_gz_raw_data__ship s
+FROM {{ ref('int_orders_margin') }} m
+LEFT JOIN {{ ref('stg_gz_raw_data__ship') }}  s
 USING (orders_id)
 )
 
 SELECT 
 orders_id
 , date_date 
-, margin + shipping_fee - log_cost - ship_cost AS operational_margin
+, margin + shipping_fee - logcost - ship_cost AS operational_margin
 FROM ship_join
